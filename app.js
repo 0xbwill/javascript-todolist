62ae01b49329c3594fc898495ba66fe195dce361
botwilly13 <william.degh@gmail.com>
2022-05-12 14:17:13 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
-// Déclaration variable
-// Saisi Todo
+// CONST declaration
 const inputTodo = document.querySelector(".todo-input");
 const pushTodo = document.querySelector(".todo-push");
-// Intéraction Todo
 const todoContainer = document.querySelector(".todo-container");
 
 // Déclaration Event Listener
@@ -11,41 +9,44 @@ todoContainer.addEventListener("click", deleteTodo);
 
 // Fonctions
 function newTodo(e) {
-  // Création de la div qui va accueilir notre tâche
+  // CREATE '<div>'
   const todoDiv = document.createElement("div");
   todoDiv.classList.add("todo");
 
-  // Création d'un <li></li>
+  // CREATE '<li>'
   const newTodo = document.createElement("li");
   newTodo.innerText = inputTodo.value;
   newTodo.classList.add("todo-item");
   todoDiv.appendChild(newTodo);
 
-  // Création d'un <button>Supprimer</button>
+  // CREATE 'Validé' button
   const checkTodo = document.createElement("button");
   checkTodo.innerHTML = "Validé";
   checkTodo.classList.add("check-btn", "btn", "btn-success");
   todoDiv.appendChild(checkTodo);
-  // Création d'un <button>Supprimer</button>
+
+  // CREATE 'Supprimer' button
   const deleteTodo = document.createElement("button");
   deleteTodo.innerHTML = "Supprimer";
   deleteTodo.classList.add("trash-btn", "btn", "btn-danger");
   todoDiv.appendChild(deleteTodo);
 
-
+  // INSERT todoDiv IN todoContainer
   todoContainer.appendChild(todoDiv);
   inputTodo.value = "";
 }
 
 function deleteTodo(e) {
-  console.log(e.target)  
+  console.log(e.target);
   const item = e.target;
-  // Si clique sur le boutton Supprimer
+
+  // CHECK 'trash-btn' pressed ?
   if (item.classList[0] === "trash-btn") {
     const todo = item.parentElement;
     todo.remove();
   }
-  // Si clique sur le bouton Validé
+
+  // CHECK 'check-btn' pressed ?
   if (item.classList[0] === "check-btn") {
     const todo = item.parentElement;
     todo.classList.toggle("validated");
